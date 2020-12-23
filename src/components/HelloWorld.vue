1a3a0d368d54d1a2d3737da2fc7953e575d949f4
unknown <bpxgy12138@163.com>
2020-12-24 00:49:34 +0800

    测试 移动端组件

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,79 @@
+<!--  -->
+<template>
+<div class="box">
+ <el-col :span="24"><div class="grid-content bg-purple-dark" >
+    <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="(item, index) in shufflingImg" :key="index">
+      <img class="imgShu" :src="item.image" alt="">
+    </el-carousel-item>
+  </el-carousel></div></el-col>
+
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+  <div>
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(item, index) in shufflingImg" :key="index">
+        <img class="imgShu" v-lazy="item.image" />
+      </van-swipe-item>
+    </van-swipe>
+  </div>
+</div>
+ 
+
+ 
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 轮播图
+   
+shufflingImg:'',
+    };
+  },
+  mounted(){
+    // 获取数据
+    let url = 'https://www.imovietrailer.com/superhero/index/carousel/list?qq=2478029182'
+    this.axios.post(url).then(({data,status}) => {
+      this.shufflingImg = data.data
+      console.log(this.shufflingImg);
+    })
+  }
+}
+
+</script>
+
+<style  scoped>
+.imgShu{
+  width: 100%;
+  height: 100%;
+}
+.box{
+  /* height:100px; px */
+
+}
+
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 20%;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: pink;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+ 
+</style>
+<style>
+ .el-carousel__container{
+    height: 6.25rem
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 配置组件
+import { Button, Swipe, SwipeItem,Lazyload  } from 'vant';
+
+Vue.use(Lazyload);
+Vue.use(Swipe);
+Vue.use(SwipeItem);
+// import 'lib-flexible'
+Vue.use(Button);
+// 导入 ElementUI
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 配置flexible
+import 'lib-flexible/flexible.js'
+// 配置axiso
+import axios from "axios"
+Vue.prototype.axios = axios
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App
+  },
+  template: '<App/>'
+})
